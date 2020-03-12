c49231d843cfa07ca99b2aabf093a88b1d0e4191
Lozd <lozd@fastmail.com>
2020-03-12 15:12:11 +0700

    Refactoring

--- resize_retina.py
+++ resize_retina.py
@@ -5,37 +5,35 @@ from tqdm import tqdm
 
 EXT = ('jpg', 'jpeg')
 IGNORE = ('contactsheet', 'cover', 'poster')
+
 root = Path('.')
 
-# list subdirectories of root
-tasks = [x for x in root.iterdir() if x.is_dir()]
-for task in tasks:
-    level = str(task)[:2]
-    if level == '00':
-        photoset = [x for x in task.iterdir() if str(x).lower().endswith(EXT)]
-        # TODO: backup option
-        # create backup dir
-        backup = task / ('original.' + level)
-        backup.mkdir(exist_ok=True)
-        print(task.name)
-        # image processing with pretty progress bar
-        for photo in tqdm(photoset):
-            img = Image.open(photo)
-            img_h, img_w = img.size
-            # do not process covers, cover-clean and contactsheets
-            # also don not process stretched images (maybe cs)
-            if (
-                (img_h / img_w > 2.25) or
-                [kw for kw in IGNORE if kw in str(photo).lower()]
-                ):
-                continue
-            # save backup
-            copyfile(photo, backup / photo.name)
-            # resize to 2880*4320 retina
-            img.thumbnail((4320, 4320), Image.BICUBIC)
-            img.save(photo, 'jpeg', quality=75, optimize=True)
-        try:
-            task.rename(str(task)[2:] + ' Mx')
-        except:
-            print("Unable to rename dir")
+# list subdirectories of root with 00names
+tasks = [x for x in root.iterdir() if (x.is_dir() and str(x)[:2] == '00')]
+for index, task in enumerate(tasks):
+    photoset = [x for x in task.iterdir() if str(x).lower().endswith(EXT)]
+    # TODO: backup option
+    # create backup dir
+    backup = task / ('original')
+    backup.mkdir(exist_ok=True)
+    print(f'{index}/{len(tasks)}: {task.name}')
+    # image processing with pretty progress bar
+    for photo in tqdm(photoset):
+        # don't process covers, cover-clean and contactsheets
+        if [kw for kw in IGNORE if kw in str(photo).lower()]:
+            continue
+        img = Image.open(photo)
+        img_h, img_w = img.size
+        # don't process stretched images (maybe cs)
+        if img_h / img_w > 2.25:
+            continue
+        # save backup
+        copyfile(photo, backup / photo.name)
+        # resize to 2880*4320 "retina"
+        img.thumbnail((4320, 4320), Image.BICUBIC)
+        img.save(photo, 'jpeg', quality=75, optimize=True)
+    try:
+        task.rename(str(task)[2:] + ' Mx')
+    except OSError:
+        print("Unable to rename dir")
 print("Done.")
