ec889cb45cc99f73663602eafe2cecd36fd69dae
Lozd <lozd@fastmail.com>
2020-03-17 17:28:13 +0700

    Add mode parameter

--- retina.py
+++ retina.py
@@ -7,7 +7,7 @@ from shutil import copyfile
 from tqdm import tqdm
 
 
-def process_images(files, backup_path=None, resize=True, webp=False):
+def process_images(files, backup_path, resize, webp):
     '''
     Process images list
 
@@ -36,27 +36,44 @@ def process_images(files, backup_path=None, resize=True, webp=False):
 
 
 def process_dir(path, backup, resize, webp):
+    '''
+    Process all image files from directory
+
+    :param path: selected path
+    :param backup: save backup
+    :param resize: resize to "retina" or just resave image
+    :param webp: convert to WebP
+    '''
     EXT = ('jpg', 'jpeg')
     # create backup dir
+    backup_path = None
     if backup:
         backup_path = path / ('originals')
         backup_path.mkdir(exist_ok=True)
-        # filter file list
-        photoset = [x for x in path.iterdir() if str(x).lower().endswith(EXT)]
-        photoset = list(filter(match_name, photoset))
-        process_images(photoset, backup_path, resize, webp)
-        try:
-            path.rename(path.parent / (path.name[2:] + ' Mx'))
-        except OSError:
-            print("Unable to rename dir")
+    # collect, filter and process
+    photoset = [x for x in path.iterdir() if str(x).lower().endswith(EXT)]
+    photoset = list(filter(match_name, photoset))
+    process_images(photoset, backup_path, resize, webp)
 
 
 def process_dirs(root, backup, resize, webp):
+    '''
+    Process all directories marked with "00"
+
+    :param root: selected root
+    :param backup: save backup
+    :param resize: resize to "retina" or just resave image
+    :param webp: convert to WebP
+    '''
     # list subdirectories of root with 00names
     tasks = [x for x in root.iterdir() if (x.is_dir() and x.name[:2] == '00')]
     for index, task in enumerate(tasks):
         print(f'{index+1}/{len(tasks)}: {task.name}')
         process_dir(task, backup, resize, webp)
+        try:
+            task.rename(task.parent / (task.name[2:] + ' Mx'))
+        except OSError:
+            print("Unable to rename dir")
     print("Done.")
 
 
@@ -66,17 +83,24 @@ def match_name(filename):
     return not [kw for kw in IGNORE if kw in filename.name.lower()]
 
 
-@click.command() # noqa: C901
+@click.command()
 @click.argument('root', type=click.Path(exists=True), required=False)
+@click.option('-d', '--dirs', 'mode', flag_value='dirs', default=True)
+@click.option('-f', '--files', 'mode', flag_value='files')
 @click.option('--backup/--no-backup', ' /-B', default=True)
 @click.option('--resize/--no-resize', ' /-R', default=True)
 @click.option('-w', '--webp', is_flag=True, default=False)
-def resize_to_retina(root, backup, resize, webp):
+def resize_to_retina(root, mode, backup, resize, webp):
     if not root:
         root = Path.cwd()
     else:
         root = Path(root)
-    process_dirs(root, backup, resize, webp)
+    if mode == 'dirs':
+        print('Processing marked dirs...')
+        process_dirs(root, backup, resize, webp)
+    elif mode == 'files':
+        print('Processing files in a directory...')
+        process_dir(root, backup, resize, webp)
 
 
 if __name__ == '__main__':
